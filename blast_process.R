d7678c4c428dab5a00c517528e03790d36a15993
WANG Haichao <wanghaichao2014@gmail.com>
2018-10-05 21:33:14 +0800

    Delete blast_process.R

--- blast_process.R
+++
@@ -1,9 +0,0 @@
-report <- read.csv(file = "report.csv")
-blast <-read.csv(file="blast_result.csv")
-sequence <-read.csv(file = "seqs.csv")
-databank<-read.csv(file = "databank.csv")
-
-merged_data<-Reduce(function(dtf1, dtf2) merge(dtf1, dtf2, by = "batch_no", all = TRUE),
-                    list(databank,report,blast,sequence))
-
-write.csv(merged_data,file = "final_merged_list.csv")
